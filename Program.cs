26e77b73ecb25ad40f102b06f63ca14d4b24fb22
makarenkoevg <makarenkoevg@mail.ru>
2023-02-07 04:21:57 +0500

    program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+
+Write("Введите массив данных через ',': ");
+string str = ReadLine();
+string[] array = str.Split(",");
+int len=3;
+
+WriteLine($"[{string.Join(",", array)}]=>[{string.Join(",", newArray(array,len))}]");
+
+string[] newArray(string[] arr, int len){
+
+    string[] outArr = new string[0];
+    int j=0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            j+=1;
+            Array.Resize(ref outArr, j);
+            outArr[j-1] = arr[i];
+        }
+
+    }
+    return outArr;
+}
